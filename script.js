5854940f588d7887fc8d695370d2e6047d36c1f7
emarkees <rphillipse@gmail.com>
2023-03-20 15:45:41 -0600

    Update menu nav-link style.css file

---
+++ script.js
@@ -0,0 +1,8 @@
+// hamburger menu
+
+
+/*document.querySelectorAll('.menuaction')
+  .forEach((n) => n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+  }));*/
