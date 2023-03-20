c993b8f3e130557c404702544370f4178842d515
emarkees <rphillipse@gmail.com>
2023-03-20 15:46:19 -0600

    Add hamburger file script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // hamburger menu
 
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('#menu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
 
 /*document.querySelectorAll('.menuaction')
   .forEach((n) => n.addEventListener('click', () => {
