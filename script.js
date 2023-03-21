82c3fa5ff2f82a45b64e98b160019a68a33706ba
emarkees <rphillipse@gmail.com>
2023-03-21 15:41:42 -0600

    Add link, feature speakers index.html files

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // hamburger menu
 
 const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('#menu');
+const navMenu = document.querySelector('.menu');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
 
-/*document.querySelectorAll('.menuaction')
+document.querySelectorAll('.menuaction')
   .forEach((n) => n.addEventListener('click', () => {
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
-  }));*/
+  }));
